16b645819cc63779dd4ae2885e1fa698c589d607
Jethro Kuan <jethrokuan95@gmail.com>
2016-02-23 09:14:04 +0800

    Partial working fzf changes

--- functions/__fzf_ctrl_r.fish
+++ functions/__fzf_ctrl_r.fish
@@ -1,6 +1,6 @@
 function __fzf_ctrl_r
-  history | eval (__fzfcmd) +s +m --tiebreak=index --toggle-sort=ctrl-r > $TMPDIR/fzf.result
-  and commandline (cat $TMPDIR/fzf.result)
+  history | eval (__fzfcmd) +s +m --tiebreak=index --toggle-sort=ctrl-r | read -l fzf_last_select [$fzf_last_select];
+  and commandline -rb $fzf_last_select
   commandline -f repaint
   rm -f $TMPDIR/fzf.result
 end
--- functions/__fzf_ctrl_t.fish
+++ functions/__fzf_ctrl_t.fish
@@ -4,9 +4,9 @@ function __fzf_ctrl_t
     -o -type f -print \
     -o -type d -print \
     -o -type l -print 2> /dev/null | sed 1d | cut -b3-"
-  eval "$FZF_CTRL_T_COMMAND | "(__fzfcmd)" -m > $TMPDIR/fzf.result"
+  eval $FZF_CTRL_T_COMMAND | fzf -m | read -laz fzf_last_select
   and sleep 0
-  and commandline -i (cat $TMPDIR/fzf.result | __fzf_escape)
+  and commandline -i $fzf_last_select
   commandline -f repaint
   rm -f $TMPDIR/fzf.result
 end
