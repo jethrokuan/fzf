6ef21c926b357e9fea1d6c75d52ec1d8c064633e
Jethro Kuan <jethrokuan95@gmail.com>
2016-02-23 16:49:35 +0800

    temp fix for uninstall errors

--- key_bindings.fish
+++ key_bindings.fish
@@ -3,9 +3,7 @@ bind \cr '__fzf_ctrl_r'
 bind \cx '__fzf_ctrl_x'
 bind \ec '__fzf_alt_c'
 
-if bind -M insert > /dev/null 2>&1
-  bind -M insert \ct '__fzf_ctrl_t'
-  bind -M insert \cr '__fzf_ctrl_r'
-  bind -M insert \cx '__fzf_ctrl_x'
-  bind -M insert \ec '__fzf_alt_c'
-end
+bind -M insert \ct '__fzf_ctrl_t'
+bind -M insert \cr '__fzf_ctrl_r'
+bind -M insert \cx '__fzf_ctrl_x'
+bind -M insert \ec '__fzf_alt_c'
