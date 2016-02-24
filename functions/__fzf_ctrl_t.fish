743bbe19480a43d13e404ac4adcc484bb0cd6810
Jethro Kuan <jethrokuan95@gmail.com>
2016-02-24 10:24:31 +0800

    Ctrl_t does not need reading an array - from @hauleths PR

--- functions/__fzf_ctrl_t.fish
+++ functions/__fzf_ctrl_t.fish
@@ -4,7 +4,7 @@ function __fzf_ctrl_t
     -o -type f -print \
     -o -type d -print \
     -o -type l -print 2> /dev/null | sed 1d | cut -b3-"
-  eval $FZF_CTRL_T_COMMAND | __fzfcmd -m | __fzfescape | read -laz selects
-  and commandline -i (for i in $selects; printf '%s ' $i; end )
+  eval "$FZF_CTRL_T_COMMAND" | __fzfcmd -m | __fzfescape | read -l selects
+  and commandline -i "$selects"
   commandline -f repaint
 end
