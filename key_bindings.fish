4d499083531c5b974df61c0b746239e48dd17eb1
Jethro Kuan <jethrokuan95@gmail.com>
2016-02-25 11:29:23 +0800

    Readded conditional after fisher fix

--- key_bindings.fish
+++ key_bindings.fish
@@ -3,7 +3,9 @@ bind \cr '__fzf_ctrl_r'
 bind \cx '__fzf_ctrl_x'
 bind \ec '__fzf_alt_c'
 
-bind -M insert \ct '__fzf_ctrl_t'
-bind -M insert \cr '__fzf_ctrl_r'
-bind -M insert \cx '__fzf_ctrl_x'
-bind -M insert \ec '__fzf_alt_c'
+if bind -M insert > /dev/null ^ /dev/null
+  bind -M insert \ct '__fzf_ctrl_t'
+  bind -M insert \cr '__fzf_ctrl_r'
+  bind -M insert \cx '__fzf_ctrl_x'
+  bind -M insert \ec '__fzf_alt_c'
+end
