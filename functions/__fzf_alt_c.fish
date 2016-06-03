51420990e0cc5f6caf78cd3d668e70576e67d3ed
Jethro Kuan <jethrokuan95@gmail.com>
2016-06-03 20:58:02 +0800

    Spin up separate fish process for fzf

--- functions/__fzf_alt_c.fish
+++ functions/__fzf_alt_c.fish
@@ -1,19 +1,19 @@
 function __fzf_alt_c
-    if not type -q fzf
-        printf "fzf not yet installed. Installing now, please wait..."
-        __fzf_install
-        commandline -f repaint
-        __fzf_alt_c
-    else
-        set -q FZF_ALT_C_COMMAND
-        or set -l FZF_ALT_C_COMMAND "
+  if not type -q fzf
+    printf "fzf not yet installed. Installing now, please wait..."
+    __fzf_install
+    commandline -f repaint
+    __fzf_alt_c
+  else
+    set -q FZF_ALT_C_COMMAND
+    or set -l FZF_ALT_C_COMMAND "
     command find -L . \\( -path '*/\\.*' -o -fstype 'dev' -o -fstype 'proc' \\) -prune \
     -o -type d -print 2> /dev/null | sed 1d | cut -b3-"
-        # Fish hangs if the command before pipe redirects (2> /dev/null)
-        eval $FZF_ALT_C_COMMAND | __fzfcmd +m | read -la select
-        if test ! (count $select) -eq 0
-            cd $select
-        end
-        commandline -f repaint
+    # Fish hangs if the command before pipe redirects (2> /dev/null)
+    fish -c "$FZF_ALT_C_COMMAND" | __fzfcmd +m | read -la select
+    if test ! (count $select) -eq 0
+      cd $select
     end
+    commandline -f repaint
+  end
 end
--- functions/__fzf_ctrl_t.fish
+++ functions/__fzf_ctrl_t.fish
@@ -11,8 +11,8 @@ function __fzf_ctrl_t
     -o -type f -print \
     -o -type d -print \
     -o -type l -print 2> /dev/null | sed 1d | cut -b3-"
-        eval "$FZF_CTRL_T_COMMAND" | __fzfcmd -m | __fzfescape | read -l selects
-        and commandline -i "$selects"
-        commandline -f repaint
+    fish -c "$FZF_CTRL_T_COMMAND" | __fzfcmd -m | __fzfescape | read -l selects
+    and commandline -i "$selects"
+    commandline -f repaint
     end
 end
