d165e1f7cbf068b3508cf7c78719e28a11d27995
Jethro Kuan <jethrokuan95@gmail.com>
2016-09-24 10:31:20 +0800

    Read CL for Ctrl-R. Fixes [#13]

--- functions/__fzf_ctrl_r.fish
+++ functions/__fzf_ctrl_r.fish
@@ -1,5 +1,5 @@
 function __fzf_ctrl_r
-  history | __fzfcmd +s +m --tiebreak=index --toggle-sort=ctrl-r | read -l select
+  history | __fzfcmd +s --tiebreak=index --toggle-sort=ctrl-r -q (commandline) | read -l select
 
   and commandline -rb $select
   commandline -f repaint
