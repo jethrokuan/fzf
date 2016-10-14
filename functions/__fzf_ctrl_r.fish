97d28f24d53fff9894fad6499be2928085139c41
Jethro Kuan <jethrokuan95@gmail.com>
2016-10-14 08:42:02 +0800

    Use custom history for multiline commands

--- functions/__fzf_ctrl_r.fish
+++ functions/__fzf_ctrl_r.fish
@@ -1,6 +1,5 @@
 function __fzf_ctrl_r
-  history | __fzfcmd +s --tiebreak=index --toggle-sort=ctrl-r -q (commandline) $FZF_CTRL_R_OPTS | read -l select
-
-  and commandline -rb $select
+  __fish_history | __fzfcmd +s --tiebreak=index --toggle-sort=ctrl-r -q (commandline) $FZF_CTRL_R_OPTS | read -l select; and
+  commandline -rb (printf $select)
   commandline -f repaint
 end
