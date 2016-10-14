889dfa7234dc2eb148604c5febc4eccfe7b428ac
Jethro Kuan <jethrokuan95@gmail.com>
2016-10-14 08:46:01 +0800

    Guard against cancels

--- functions/__fzf_ctrl_r.fish
+++ functions/__fzf_ctrl_r.fish
@@ -1,5 +1,7 @@
 function __fzf_ctrl_r
-  __fish_history | __fzfcmd +s --tiebreak=index --toggle-sort=ctrl-r -q (commandline) $FZF_CTRL_R_OPTS | read -l select; and
-  commandline -rb (printf $select)
-  commandline -f repaint
+  __fish_history | __fzfcmd +s --tiebreak=index --toggle-sort=ctrl-r -q (commandline) $FZF_CTRL_R_OPTS | read -l select 
+  if not test -z $select
+    commandline -rb (printf $select)
+    commandline -f repaint
+  end
 end
