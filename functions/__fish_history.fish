c1f664146f3f7e3cd65f018f01119425c181dd76
Jethro Kuan <jethrokuan95@gmail.com>
2016-10-18 07:55:19 +0800

    Check if tac exists, if not use tail -r

--- functions/__fish_history.fish
+++ functions/__fish_history.fish
@@ -1,8 +1,15 @@
 function __fish_history
+  set -l QUERY_CMD
+  if type -q tac
+    set QUERY_CMD "tac"
+  else
+    set QUERY_CMD "tail -r"
+  end
+  
   if test -e ~/.config/fish/fish_history
-    tac ~/.config/fish/fish_history | grep "^\- cmd: " | sed 's/\- cmd: //' 
+    eval $QUERY_CMD ~/.config/fish/fish_history | grep "^\- cmd: " | sed 's/\- cmd: //' 
   else if test -e ~/.local/share/fish/fish_history
-    tac ~/.local/share/fish/fish_history | grep "^\- cmd: " | sed 's/\- cmd: //'
+    eval $QUERY_CMD ~/.local/share/fish/fish_history | grep "^\- cmd: " | sed 's/\- cmd: //'
   else
     history
   end
