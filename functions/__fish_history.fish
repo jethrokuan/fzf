76ef110f5288bf33d0f5c227b0a04f333f4720b3
Jethro Kuan <jethrokuan95@gmail.com>
2016-10-17 21:03:07 +0800

    use tac instead

--- functions/__fish_history.fish
+++ functions/__fish_history.fish
@@ -1,8 +1,8 @@
 function __fish_history
   if test -e ~/.config/fish/fish_history
-    cat ~/.config/fish/fish_history | grep "^\- cmd: " | sed 's/\- cmd: //' 
+    tac ~/.config/fish/fish_history | grep "^\- cmd: " | sed 's/\- cmd: //' 
   else if test -e ~/.local/share/fish/fish_history
-    cat ~/.local/share/fish/fish_history | grep "^\- cmd: " | sed 's/\- cmd: //'
+    tac ~/.local/share/fish/fish_history | grep "^\- cmd: " | sed 's/\- cmd: //'
   else
     history
   end
