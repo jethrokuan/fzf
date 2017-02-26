bf0af7188e2fcbaf47230a704252394d93b6eeaa
Jethro Kuan <jethrokuan95@gmail.com>
2017-02-26 10:29:29 +0800

    Support multi-select for __fzf_find_file

--- functions/__fzf_find_file.fish
+++ functions/__fzf_find_file.fish
@@ -5,7 +5,13 @@ function __fzf_find_file
   -o -type f -print \
   -o -type d -print \
   -o -type l -print 2> /dev/null | sed 1d | cut -b3-"
-  fish -c "$FZF_FIND_FILE_COMMAND" | __fzfcmd -m $FZF_FIND_FILE_OPTS | read -l selects
-  and commandline -i "\"$selects\""
+  fish -c "$FZF_FIND_FILE_COMMAND" | __fzfcmd -m $FZF_FIND_FILE_OPTS | while read -l s; set selects $selects $s; end
+  if not test "$dir" = "."
+    commandline -t ""
+  end
+  for select in $selects
+    commandline -it -- "\"$select\""
+    commandline -it -- " "
+  end
   commandline -f repaint
 end
