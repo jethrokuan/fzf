67b52003b4509d06140eb82fa6be801079f62937
Jethro Kuan <jethrokuan95@gmail.com>
2017-02-26 11:24:39 +0800

    Fix __fzf_reverse_isearch failing on certain histories
    - Fix #33

--- functions/__fzf_reverse_isearch.fish
+++ functions/__fzf_reverse_isearch.fish
@@ -1,7 +1,7 @@
 function __fzf_reverse_isearch
-  __fish_history | __fzfcmd +s --tiebreak=index --toggle-sort=ctrl-r -q (commandline) $FZF_DEFAULT_OPTS $FZF_REVERSE_ISEARCH_OPTS | read -l select 
+  __fish_history | __fzfcmd +s --tiebreak=index --toggle-sort=ctrl-r -q (commandline) $FZF_DEFAULT_OPTS $FZF_REVERSE_ISEARCH_OPTS | read -l select
   if not test -z $select
-    commandline -rb (printf $select)
+    commandline -rb "$select"
     commandline -f repaint
   end
 end
