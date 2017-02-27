dc112f73a0385998217779033948dac7bb0fa205
Jethro Kuan <jethrokuan95@gmail.com>
2017-02-27 08:00:10 +0800

    Cleanup _fzf_find_file

--- functions/__fzf_find_file.fish
+++ functions/__fzf_find_file.fish
@@ -1,14 +1,11 @@
 function __fzf_find_file
   set -q FZF_FIND_FILE_COMMAND
-  or set -l FZF_FIND_FILE_COMMAND "
-  command find -L . \\( -path '*/\\.*' -o -fstype 'dev' -o -fstype 'proc' \\) -prune \
-  -o -type f -print \
-  -o -type d -print \
-  -o -type l -print 2> /dev/null | sed 1d | cut -b3-"
+  or set -l FZF_FIND_FILE_COMMAND
+  "command find -L \$dir -mindepth 1 \\( -path \$dir'*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' \\) -prune \
+    -o -type f -print \
+    -o -type d -print \
+    -o -type l -print 2> /dev/null | cut -b3-"
   fish -c "$FZF_FIND_FILE_COMMAND" | __fzfcmd -m $FZF_DEFAULT_OPTS $FZF_FIND_FILE_OPTS | while read -l s; set selects $selects $s; end
-  if not test "$dir" = "."
-    commandline -t ""
-  end
   for select in $selects
     commandline -it -- "\"$select\""
     commandline -it -- " "
