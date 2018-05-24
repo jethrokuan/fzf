3faa74a1895f24cbff7aa9ce50d154d4de59581c
Junjue Wang <junjuew@cs.cmu.edu>
2018-05-24 12:01:41 -0400

    add in keybinding and open functions

---
+++ functions/__fzf_open.fish
@@ -0,0 +1,26 @@
+function __fzf_open -d "Open files and directories"
+    set -l commandline (__fzf_parse_commandline)
+    set -l dir $commandline[1]
+    set -l fzf_query $commandline[2]
+
+    set -l options  "e/editor"
+
+    set -q FZF_OPEN_COMMAND
+    or set -l FZF_OPEN_COMMAND "
+    command find -L \$dir -mindepth 1 \\( -path \$dir'*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' \\) -prune \
+    -o -type f -print \
+    -o -type d -print \
+    -o -type l -print 2> /dev/null | sed 's@^\./@@'"
+
+    eval "$FZF_OPEN_COMMAND | "(__fzfcmd) "-m $FZF_DEFAULT_OPTS $FZF_FIND_FILE_OPTS --query \"$fzf_query\"" | read -l select
+
+    if not test -z "$select"
+        if set -q _flag_editor
+            eval "$EDITOR $select"
+        else
+            open $select
+        commandline -t ""
+    end
+
+    commandline -f repaint
+end
