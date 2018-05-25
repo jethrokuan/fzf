7b0cc5d6dba67bda0b6912aadc31dd65ada955e4
Junjue Wang <junjuew@cs.cmu.edu>
2018-05-25 11:16:27 -0400

    test xdg-open and open before issuing open cmd

--- functions/__fzf_open.fish
+++ functions/__fzf_open.fish
@@ -1,4 +1,12 @@
-function __fzf_open -d "Open files and directories"
+function __fzf_open -d "Open files and directories."
+    function __fzf_open_get_open_cmd -d "Find appropriate open command."
+        if type -q xdg-open
+            echo "xdg-open"
+        else if type -q open
+            echo "open"
+        end
+    end
+
     set -l commandline (__fzf_parse_commandline)
     set -l dir $commandline[1]
     set -l fzf_query $commandline[2]
@@ -16,12 +24,19 @@ function __fzf_open -d "Open files and directories"
 
     eval "$FZF_OPEN_COMMAND | "(__fzfcmd) "-m $FZF_DEFAULT_OPTS $FZF_OPEN_OPTS --query \"$fzf_query\"" | read -l select
 
-    if not test -z "$select"
-        if set -q _flag_editor
-            eval "$EDITOR $select"
-        else
-            open $select
+    # set how to open
+    set -l open_cmd
+    if set -q _flag_editor
+        set open_cmd "$EDITOR"
+    else
+        set open_cmd (__fzf_open_get_open_cmd)
+        if test -z "$open_cmd"
+            echo "Couldn't find appropriate open command to use. Do you have 'xdg-open' or 'open' installed?"; and return 1
         end
+    end
+
+    if not test -z "$select"
+        eval "$open_cmd $select"
         commandline -t ""
     end
 
