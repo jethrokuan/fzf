f8b1cb7e66948d088179a34a5903edd99bc6bac7
Zeeko <vaezt@outlook.com>
2019-05-30 18:05:54 +0800

    Update __fzf_complete.fish

--- functions/__fzf_complete.fish
+++ functions/__fzf_complete.fish
@@ -98,7 +98,7 @@ function __fzf_complete -d 'fzf completion and print selection back to commandli
             case '~'
                 commandline -t -- (string sub -s 2 (string escape -n -- $r))
             case '*'
-                commandline -t -- (string escape -n -- $r)
+                commandline -t -- $r
         end
         [ $i -lt (count $result) ]; and commandline -i ' '
     end
