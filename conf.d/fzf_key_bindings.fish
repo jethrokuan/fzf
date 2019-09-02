ea1a4276f0259cf625fc9cb06108c64a0b526b2e
Jethro Kuan <jethrokuan95@gmail.com>
2019-09-02 10:22:39 +0800

    Update keybindings
    See #119 for motivation.

--- conf.d/fzf_key_bindings.fish
+++ conf.d/fzf_key_bindings.fish
@@ -15,20 +15,20 @@ if test "$FZF_LEGACY_KEYBINDINGS" -eq 1
         bind -M insert \co '__fzf_open --editor'
     end
 else
-    bind \cf '__fzf_find_file'
+    bind \co '__fzf_find_file'
     bind \cr '__fzf_reverse_isearch'
-    bind \eo '__fzf_cd'
-    bind \eO '__fzf_cd --hidden'
-    bind \cg '__fzf_open'
-    bind \co '__fzf_open --editor'
+    bind \ec '__fzf_cd'
+    bind \eC '__fzf_cd --hidden'
+    bind \eo '__fzf_open'
+    bind \eo '__fzf_open --editor'
 
     if bind -M insert >/dev/null 2>/dev/null
-        bind -M insert \cf '__fzf_find_file'
+        bind -M insert \co '__fzf_find_file'
         bind -M insert \cr '__fzf_reverse_isearch'
-        bind -M insert \eo '__fzf_cd'
-        bind -M insert \eO '__fzf_cd --hidden'
-        bind -M insert \cg '__fzf_open'
-        bind -M insert \co '__fzf_open --editor'
+        bind -M insert \ec '__fzf_cd'
+        bind -M insert \eC '__fzf_cd --hidden'
+        bind -M insert \eo '__fzf_open'
+        bind -M insert \eo '__fzf_open --editor'
     end
 end
 
