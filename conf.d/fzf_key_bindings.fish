25e6e181f3243f4382811b5e3ac005cdb64a03bd
Leonora Tindall <nora@nora.codes>
2019-10-28 10:01:39 -0500

    Use Ctrl-P, Alt-P, Alt-Shift-P instead of O

--- conf.d/fzf_key_bindings.fish
+++ conf.d/fzf_key_bindings.fish
@@ -15,20 +15,20 @@ if test "$FZF_LEGACY_KEYBINDINGS" -eq 1
         bind -M insert \co '__fzf_open --editor'
     end
 else
-    bind \co '__fzf_find_file'
+    bind \cp '__fzf_find_file'
     bind \cr '__fzf_reverse_isearch'
     bind \ec '__fzf_cd'
     bind \eC '__fzf_cd --hidden'
-    bind \eO '__fzf_open'
-    bind \eo '__fzf_open --editor'
+    bind \eP '__fzf_open'
+    bind \ep '__fzf_open --editor'
 
     if bind -M insert >/dev/null 2>/dev/null
-        bind -M insert \co '__fzf_find_file'
+        bind -M insert \cp '__fzf_find_file'
         bind -M insert \cr '__fzf_reverse_isearch'
         bind -M insert \ec '__fzf_cd'
         bind -M insert \eC '__fzf_cd --hidden'
-        bind -M insert \eO '__fzf_open'
-        bind -M insert \eo '__fzf_open --editor'
+        bind -M insert \eP '__fzf_open'
+        bind -M insert \ep '__fzf_open --editor'
     end
 end
 
