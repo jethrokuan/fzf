6ba53fd0deb63b5e3ff424771f7edbf87fe1d240
Eric Bode <eric.bode@globalorange.nl>
2020-03-18 18:00:09 +0100

    Check if fzf package has key bindings installed

--- key_bindings.fish
+++ key_bindings.fish
@@ -1,5 +1,11 @@
 if not functions -q fzf_key_bindings
   source $OMF_PATH/pkg/fzf/conf.d/fzf_key_bindings.fish
 else
-  fzf_key_bindings
+  if functions -D fzf_key_bindings | string match -r ".+vendor_functions\.d.+" -q 
+    functions -c fzf_key_bindings fzf_key_bindings_vendor
+    function fzf_key_bindings
+      source $OMF_PATH/pkg/fzf/conf.d/fzf_key_bindings.fish
+    end
+    fzf_key_bindings
+  end
 end
