b7f79784c2695c222c8e5a1e55912d6b515fae09
Greg Anders <8965202+gpanders@users.noreply.github.com>
2020-05-22 10:52:46 -0600

    Add option to disable keybindings

--- conf.d/fzf_key_bindings.fish
+++ conf.d/fzf_key_bindings.fish
@@ -1,41 +1,43 @@
-if test "$FZF_LEGACY_KEYBINDINGS" -eq 1
-    bind \ct '__fzf_find_file'
-    bind \cr '__fzf_reverse_isearch'
-    bind \ec '__fzf_cd'
-    bind \eC '__fzf_cd --hidden'
-    bind \cg '__fzf_open'
-    bind \co '__fzf_open --editor'
+if test "$FZF_DISABLE_KEYBINDINGS" -ne 1
+    if test "$FZF_LEGACY_KEYBINDINGS" -eq 1
+        bind \ct '__fzf_find_file'
+        bind \cr '__fzf_reverse_isearch'
+        bind \ec '__fzf_cd'
+        bind \eC '__fzf_cd --hidden'
+        bind \cg '__fzf_open'
+        bind \co '__fzf_open --editor'
 
-    if bind -M insert >/dev/null 2>/dev/null
-        bind -M insert \ct '__fzf_find_file'
-        bind -M insert \cr '__fzf_reverse_isearch'
-        bind -M insert \ec '__fzf_cd'
-        bind -M insert \eC '__fzf_cd --hidden'
-        bind -M insert \cg '__fzf_open'
-        bind -M insert \co '__fzf_open --editor'
+        if bind -M insert >/dev/null 2>/dev/null
+            bind -M insert \ct '__fzf_find_file'
+            bind -M insert \cr '__fzf_reverse_isearch'
+            bind -M insert \ec '__fzf_cd'
+            bind -M insert \eC '__fzf_cd --hidden'
+            bind -M insert \cg '__fzf_open'
+            bind -M insert \co '__fzf_open --editor'
+        end
+    else
+        bind \co '__fzf_find_file'
+        bind \cr '__fzf_reverse_isearch'
+        bind \ec '__fzf_cd'
+        bind \eC '__fzf_cd --hidden'
+        bind \eO '__fzf_open'
+        bind \eo '__fzf_open --editor'
+
+        if bind -M insert >/dev/null 2>/dev/null
+            bind -M insert \co '__fzf_find_file'
+            bind -M insert \cr '__fzf_reverse_isearch'
+            bind -M insert \ec '__fzf_cd'
+            bind -M insert \eC '__fzf_cd --hidden'
+            bind -M insert \eO '__fzf_open'
+            bind -M insert \eo '__fzf_open --editor'
+        end
     end
-else
-    bind \co '__fzf_find_file'
-    bind \cr '__fzf_reverse_isearch'
-    bind \ec '__fzf_cd'
-    bind \eC '__fzf_cd --hidden'
-    bind \eO '__fzf_open'
-    bind \eo '__fzf_open --editor'
 
-    if bind -M insert >/dev/null 2>/dev/null
-        bind -M insert \co '__fzf_find_file'
-        bind -M insert \cr '__fzf_reverse_isearch'
-        bind -M insert \ec '__fzf_cd'
-        bind -M insert \eC '__fzf_cd --hidden'
-        bind -M insert \eO '__fzf_open'
-        bind -M insert \eo '__fzf_open --editor'
-    end
-end
-
-if set -q FZF_COMPLETE
-    bind \t '__fzf_complete'
-    if bind -M insert >/dev/null 2>/dev/null
-        bind -M insert \t '__fzf_complete'
+    if set -q FZF_COMPLETE
+        bind \t '__fzf_complete'
+        if bind -M insert >/dev/null 2>/dev/null
+            bind -M insert \t '__fzf_complete'
+        end
     end
 end
 
