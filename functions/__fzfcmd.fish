3c95fe9aa3533030cb3c5d9bd256e80a220af41d
Kid <44045911+kidonng@users.noreply.github.com>
2020-12-07 21:25:37 +0800

    Use environment variables

--- functions/__fzfcmd.fish
+++ functions/__fzfcmd.fish
@@ -2,8 +2,8 @@ function __fzfcmd
     set -q FZF_TMUX; or set FZF_TMUX 0
     set -q FZF_TMUX_HEIGHT; or set FZF_TMUX_HEIGHT 40%
     if [ $FZF_TMUX -eq 1 ]
-        echo "fzf-tmux -d$FZF_TMUX_HEIGHT"
+        echo "$FZF_TMUX_CMD -d$FZF_TMUX_HEIGHT"
     else
-        echo "fzf"
+        echo "$FZF_CMD"
     end
 end
