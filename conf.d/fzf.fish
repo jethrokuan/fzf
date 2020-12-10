e426c15fc2b5471e24e7e51ac212b5d09c6e1b2b
Jorge Bucaran <mail@jorgebucaran.com>
2020-12-10 21:07:28 +0900

    Remove empty unused uninstall handler

--- conf.d/fzf.fish
+++ conf.d/fzf.fish
@@ -4,12 +4,3 @@ set -q FZF_LEGACY_KEYBINDINGS; or set -U FZF_LEGACY_KEYBINDINGS 1
 set -q FZF_DISABLE_KEYBINDINGS; or set -U FZF_DISABLE_KEYBINDINGS 0
 set -q FZF_PREVIEW_FILE_CMD; or set -U FZF_PREVIEW_FILE_CMD "head -n 10"
 set -q FZF_PREVIEW_DIR_CMD; or set -U FZF_PREVIEW_DIR_CMD "ls"
-
-function fzf_uninstall -e fzf_uninstall
-    # disabled until we figure out a sensible way to ensure user overrides
-    # are not erased
-    # set -l _vars (set | command grep -E "^FZF.*\$" | command awk '{print $1;}')
-    # for var in $_vars
-    #     eval (set -e $var)
-    # end
-end
